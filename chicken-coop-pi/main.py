2bb5fcc547d263ba91c5005de70fe0347ce8489a
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-01-08 15:35:00 +0100

    more improvements for Pi script

--- chicken-coop-pi/main.py
+++ chicken-coop-pi/main.py
@@ -8,16 +8,17 @@ IN3 = 22
 IN4 = 23
 
 # careful lowering this, at some point you run into the mechanical limitation of how quick
-# your motor can move
-STEP_SLEEP = 0.002
+# your motor can move. 0.005 is about the found minimum
+STEP_SLEEP = 0.005
 
 STEP_COUNT = 4096  # 5.625*(1/64) per step, 4096 steps is 360°
 
-direction = True  # True for clockwise, False for counter-clockwise
+# True for clockwise, False for counter-clockwise (when looking in motor direction)
+DIRECTION = False
 
 # defining stepper motor sequence (found in
 # documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
-step_sequence = [[1, 0, 0, 0],
+STEP_SEQUENCE = [[1, 0, 0, 0],
                  [1, 0, 1, 0],
                  [0, 0, 1, 0],
                  [0, 1, 1, 0],
@@ -25,13 +26,15 @@ step_sequence = [[1, 0, 0, 0],
                  [0, 1, 0, 1],
                  [0, 0, 0, 1],
                  [1, 0, 0, 1]]
-motor_pins = [in1, in3, in2, in4]
+MOTOR_PINS = [IN1, IN3, IN2, IN4]
 
 
 def main():
     print("-- Chicken Coop Control --")
-    print("INx pin to BCM mapping:")
-    print(f"IN1 -> {in1} | IN2 -> {in2} | IN3 -> {in3} | IN4 -> {in4}")
+    print("INx pin to BCM mapping: ", end="")
+    print(f"IN1 -> {IN1} | IN2 -> {IN2} | IN3 -> {IN3} | IN4 -> {IN4}")
+    dir_str = "Clockwise" if DIRECTION else "Counter-Clockwise"
+    print(f"Direction: {dir_str}")
     motor_step_counter = 0
     # setting up
     GPIO.setmode(GPIO.BCM)
@@ -50,23 +53,20 @@ def main():
         # the meat
         try:
             for i in range(STEP_COUNT):
-                for pin in range(0, len(motor_pins)):
-                    GPIO.output(motor_pins[pin], step_sequence[motor_step_counter][pin])
-                if direction:
+                for pin in range(0, len(MOTOR_PINS)):
+                    GPIO.output(MOTOR_PINS[pin], STEP_SEQUENCE[motor_step_counter][pin])
+                if DIRECTION:
                     motor_step_counter = (motor_step_counter - 1) % 8
-                elif not direction:
+                elif not DIRECTION:
                     motor_step_counter = (motor_step_counter + 1) % 8
                 else:  # defensive programming
-                    print("uh oh... direction should *always* be either True or False")
+                    print("Error: Invalid direction")
                     cleanup()
                     exit(1)
                 time.sleep(STEP_SLEEP)
         except KeyboardInterrupt:
             cleanup()
             exit(1)
-
-    cleanup()
-    exit(0)
 
 
 def cleanup():
